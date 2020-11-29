728d1ddd10c02e6cb73c21f5bf6d76b5173d91d9
Ketsia-a <iraketsia2001@gmail.com>
2020-11-29 07:29:29 +0200

    making tests for the two classes

---
+++ news_test.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import News
+
+class NewsTest(unittest.TestCase):
+        
+    '''
+    Test class to test the behaviour of the Sources class
+    '''
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Sources("aftenposten","Aftenposten","Norges ledende nettavis med alltid oppdaterte nyheter innenfor innenriks, utenriks, sport og kultur.", "https://www.aftenposten.no","general")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Sources))
+
+
+if __name__ == '__main__':
+    unittest.main()
