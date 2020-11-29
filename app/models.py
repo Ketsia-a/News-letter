e3657526d2339f58f9d6b0fce837c22328dba3f8
Ketsia-a <iraketsia2001@gmail.com>
2020-11-29 16:44:19 +0200

    removing comments

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class News:
         self.category = category
 
 class Article:
-    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt,text):
+    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt):
         self.id = id
         self.name = name
         self.author = author
@@ -16,4 +16,5 @@ class Article:
         self.url = url
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
-        self.text = text
+   
+        
