080b8855934c0d51bc16711de149cfabcf8abbff
Ketsia-a <iraketsia2001@gmail.com>
2020-11-29 07:49:28 +0200

    creating an appfolder

---
+++ app/models.py
@@ -0,0 +1,19 @@
+class News:
+    def __init__(self,id,name,description,url,category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+
+class Article:
+    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt,text):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.text = text
