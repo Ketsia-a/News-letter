09f40ee5b8f51f2b5eca7cba1dbbcd9d6b9047b8
Ketsia-a <iraketsia2001@gmail.com>
2020-11-29 07:51:30 +0200

    importing os and adding the URLs

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    SOURCE_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'                 
+    ARTICLE_API_NEWS_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
